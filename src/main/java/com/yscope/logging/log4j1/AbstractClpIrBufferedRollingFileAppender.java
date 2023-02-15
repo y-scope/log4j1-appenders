66f64a4f4bbce7abe54547dcb2bdb8b272b53d64
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2023-02-15 10:26:53 -0500

    Whitespace fixes.

--- src/main/java/com/yscope/logging/log4j1/AbstractClpIrBufferedRollingFileAppender.java
+++ src/main/java/com/yscope/logging/log4j1/AbstractClpIrBufferedRollingFileAppender.java
@@ -10,117 +10,116 @@ import java.io.IOException;
  * functionality
  */
 public abstract class AbstractClpIrBufferedRollingFileAppender
-        extends AbstractBufferedRollingFileAppender
-{
-    // File size based rollover strategy for streaming compressed logging is
-    // governed by both the compressed on-disk size and the size of raw
-    // uncompressed content. The former is to ensure a reasonable local and/or
-    // remote file size to reduce both the file system overhead and cost during
-    // log generation, synchronization with remote log store, accessing and
-    // searching the compressed log file at a later time. The uncompressed size
-    // is also used to ensure that compressed log files when decompressed back
-    // to its original content be opened efficiently by file editors.
-    private long compressedRolloverSize = 16 * 1024 * 1024;
-    private long compressedSizeSinceLastRollover = 0L;
+    extends AbstractBufferedRollingFileAppender {
+  // File size based rollover strategy for streaming compressed logging is
+  // governed by both the compressed on-disk size and the size of raw
+  // uncompressed content. The former is to ensure a reasonable local and/or
+  // remote file size to reduce both the file system overhead and cost during
+  // log generation, synchronization with remote log store, accessing and
+  // searching the compressed log file at a later time. The uncompressed size
+  // is also used to ensure that compressed log files when decompressed back
+  // to its original content be opened efficiently by file editors.
+  private long compressedRolloverSize = 16 * 1024 * 1024;
+  private long compressedSizeSinceLastRollover = 0L;
 
-    private long uncompressedRolloverSize = 2L * 1024 * 1024 * 1024;
-    private long uncompressedSizeSinceLastRollover = 0L;
+  private long uncompressedRolloverSize = 2L * 1024 * 1024 * 1024;
+  private long uncompressedSizeSinceLastRollover = 0L;
 
-    // CLP streaming compression parameters
-    private int compressionLevel = 3;
-    private boolean closeFrameOnFlush = true;
-    private boolean useCompactEncoding = false;
+  // CLP streaming compression parameters
+  private int compressionLevel = 3;
+  private boolean closeFrameOnFlush = true;
+  private boolean useCompactEncoding = false;
 
-    protected String baseName;
-    protected ClpIrFileAppender clpIrFileAppender = null;
-    protected String currentFileName;
-    protected String outputDir;
+  protected String baseName;
+  protected ClpIrFileAppender clpIrFileAppender = null;
+  protected String currentFileName;
+  protected String outputDir;
 
-    public static final String CLP_COMPRESSED_IRSTREAM_FILE_EXTENSION = ".clp.zst";
+  public static final String CLP_COMPRESSED_IRSTREAM_FILE_EXTENSION = ".clp.zst";
 
-    public void setCompressedRolloverSize(long compressedRolloverSize) {
-        this.compressedRolloverSize = compressedRolloverSize;
+  public void setCompressedRolloverSize (long compressedRolloverSize) {
+    this.compressedRolloverSize = compressedRolloverSize;
+  }
+
+  public void setUncompressedRolloverSize (long uncompressedRolloverSize) {
+    this.uncompressedRolloverSize = uncompressedRolloverSize;
+  }
+
+  public void setUseCompactEncoding (boolean useCompactEncoding) {
+    this.useCompactEncoding = useCompactEncoding;
+  }
+
+  public void setOutputDir (String outputDir) {
+    this.outputDir = outputDir;
+  }
+
+  public void setBaseName (String baseName) {
+    this.baseName = baseName;
+  }
+
+  public void setCloseFrameOnFlush (boolean closeFrameOnFlush) {
+    this.closeFileOnShutdown = closeFrameOnFlush;
+  }
+
+  public long getUncompressedSize () {
+    return uncompressedSizeSinceLastRollover + clpIrFileAppender.getUncompressedSize();
+  }
+
+  public long getCompressedSize () {
+    return compressedSizeSinceLastRollover + clpIrFileAppender.getCompressedSize();
+  }
+
+  @Override
+  public void activateOptions () {
+    super.activateOptions();
+    try {
+      clpIrFileAppender = new ClpIrFileAppender(currentLogPath, layout, useCompactEncoding,
+                                                closeFrameOnFlush, compressionLevel);
+    } catch (IOException e) {
+      logError("Failed to activate appender", e);
     }
+  }
 
-    public void setUncompressedRolloverSize(long uncompressedRolloverSize) {
-        this.uncompressedRolloverSize = uncompressedRolloverSize;
-    }
+  @Override
+  public void appendBufferedFile (LoggingEvent loggingEvent) {
+    clpIrFileAppender.append(loggingEvent);
+  }
 
-    public void setUseCompactEncoding(boolean useCompactEncoding) {
-        this.useCompactEncoding = useCompactEncoding;
-    }
+  @Override
+  protected boolean rolloverRequired () {
+    return getCompressedSize() > compressedRolloverSize
+        || getUncompressedSize() > uncompressedRolloverSize;
+  }
 
-    public void setOutputDir(String outputDir) {
-        this.outputDir = outputDir;
-    }
+  @Override
+  protected void closeBufferedAppender () {
+    clpIrFileAppender.close();
+  }
 
-    public void setBaseName(String baseName) {
-        this.baseName = baseName;
-    }
+  protected void updateLogFileName () {
+    currentFileName = baseName + "." + lastRolloverTimestamp
+        + CLP_COMPRESSED_IRSTREAM_FILE_EXTENSION;
+  }
 
-    public void setCloseFrameOnFlush(boolean closeFrameOnFlush) {
-        this.closeFileOnShutdown = closeFrameOnFlush;
-    }
+  @Override
+  protected void updateLogFilePath () {
+    updateLogFileName();
+    currentLogPath = outputDir + "/" + currentFileName;
+  }
 
-    public long getUncompressedSize() {
-        return uncompressedSizeSinceLastRollover + clpIrFileAppender.getUncompressedSize();
+  @Override
+  protected void startNewBufferedFile () {
+    try {
+      compressedRolloverSize += clpIrFileAppender.getCompressedSize();
+      uncompressedSizeSinceLastRollover += clpIrFileAppender.getUncompressedSize();
+      clpIrFileAppender.startNewFile(currentLogPath);
+    } catch (IOException e) {
+      logError("Failed to start new buffered file", e);
     }
+  }
 
-    public long getCompressedSize() {
-        return compressedSizeSinceLastRollover + clpIrFileAppender.getCompressedSize();
-    }
-
-    @Override
-    public void activateOptions() {
-        super.activateOptions();
-        try {
-            clpIrFileAppender = new ClpIrFileAppender(currentLogPath, layout, useCompactEncoding,
-                    closeFrameOnFlush, compressionLevel);
-        } catch (IOException e) {
-            logError("Failed to activate appender", e);
-        }
-    }
-
-    @Override
-    public void appendBufferedFile(LoggingEvent loggingEvent) {
-        clpIrFileAppender.append(loggingEvent);
-    }
-
-    @Override
-    protected boolean rolloverRequired() {
-        return getCompressedSize() > compressedRolloverSize ||
-                getUncompressedSize() > uncompressedRolloverSize;
-    }
-
-    @Override
-    protected void closeBufferedAppender() {
-        clpIrFileAppender.close();
-    }
-
-    protected void updateLogFileName() {
-        currentFileName = baseName + "." + lastRolloverTimestamp
-                + CLP_COMPRESSED_IRSTREAM_FILE_EXTENSION;
-    }
-
-    @Override
-    protected void updateLogFilePath() {
-        updateLogFileName();
-        currentLogPath = outputDir + "/" + currentFileName;
-    }
-
-    @Override
-    protected void startNewBufferedFile() {
-        try {
-            compressedRolloverSize += clpIrFileAppender.getCompressedSize();
-            uncompressedSizeSinceLastRollover += clpIrFileAppender.getUncompressedSize();
-            clpIrFileAppender.startNewFile(currentLogPath);
-        } catch (IOException e) {
-            logError("Failed to start new buffered file", e);
-        }
-    }
-
-    @Override
-    public void flush() throws IOException {
-        clpIrFileAppender.flush();
-    }
+  @Override
+  public void flush () throws IOException {
+    clpIrFileAppender.flush();
+  }
 }
