49208815fd5c00b851f79a5acccd5f533b8f9aba
Jack Luo <jack.luo@mail.utoronto.ca>
2023-02-26 19:10:15 -0800

    Print timestamp in EnhancedAppenderSkeleton's internal logging methods. (#4)

--- src/main/java/com/yscope/logging/log4j1/EnhancedAppenderSkeleton.java
+++ src/main/java/com/yscope/logging/log4j1/EnhancedAppenderSkeleton.java
@@ -1,6 +1,9 @@
 package com.yscope.logging.log4j1;
 
+import java.util.Date;
+
 import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.helpers.ISO8601DateFormat;
 import org.apache.log4j.helpers.LogLog;
 
 /**
@@ -8,27 +11,33 @@ import org.apache.log4j.helpers.LogLog;
  * prepend the name of the logger.
  */
 public abstract class EnhancedAppenderSkeleton extends AppenderSkeleton {
+  private static final ISO8601DateFormat timestampFormatter = new ISO8601DateFormat();
+
   protected void logDebug (String msg) {
-    LogLog.debug("[" + name + "] " + msg);
+    LogLog.debug(getCurrentTimestampString() + " [" + name + "] " + msg);
   }
 
   protected void logDebug (String msg, Throwable t) {
-    LogLog.debug("[" + name + "] " + msg, t);
+    LogLog.debug(getCurrentTimestampString() + " [" + name + "] " + msg, t);
   }
 
   protected void logWarn (String msg) {
-    LogLog.warn("[" + name + "] " + msg);
+    LogLog.warn(getCurrentTimestampString() + " [" + name + "] " + msg);
   }
 
   protected void logWarn (String msg, Throwable t) {
-    LogLog.warn("[" + name + "] " + msg, t);
+    LogLog.warn(getCurrentTimestampString() + " [" + name + "] " + msg, t);
   }
 
   protected void logError (String msg) {
-    LogLog.error("[" + name + "] " + msg);
+    LogLog.error(getCurrentTimestampString() + " [" + name + "] " + msg);
   }
 
   protected void logError (String msg, Throwable t) {
-    LogLog.error("[" + name + "] " + msg, t);
+    LogLog.error(getCurrentTimestampString() + " [" + name + "] " + msg, t);
+  }
+
+  private static String getCurrentTimestampString () {
+    return timestampFormatter.format(new Date());
   }
 }
